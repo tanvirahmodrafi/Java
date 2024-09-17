a293d4303d1f580b4b753f595812d8167cbd9c2f
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2024-09-17 07:42:19 +0600

    up

--- lab.java
+++ lab.java
@@ -1,36 +1,49 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 public class lab {
-    public static int pranto(int a){
-        return a;
+    public lab() {
     }
-    public static void main(String[] args) {
-        marks(65);
+ 
+    public static int pranto(int var0) {
+       return var0;
     }
-    static void abc(int a){
-        System.out.println(a);
+ 
+    public static void main(String[] var0) {
+       marks(65);
     }
-    static void  ifs(int a , int b){
-        System.out.println(a+b);
+ 
+    static void abc(int var0) {
+       System.out.println(var0);
     }
-    static void ifg(int a, int b, int c){
-        System.out.println(a*b*c);
+ 
+    static void ifs(int var0, int var1) {
+       System.out.println(var0 + var1);
     }
-    static int mul(int a , int b,int c){
-        return(a*b*c);
+ 
+    static void ifg(int var0, int var1, int var2) {
+       System.out.println(var0 * var1 * var2);
     }
-    static boolean GenZ(int a ){
-        return(1997<a);
+ 
+    static int mul(int var0, int var1, int var2) {
+       return var0 * var1 * var2;
     }
-    static void marks(int a){
-        if(a>=80){
-            System.out.println("A+");
-        }else if(a>=75){
-            System.out.println("A");
-        }else if(a>=70){
-            System.out.println("A-");
-        }else if(a>=65){
-            System.out.println("B+");
-        }else{
-            System.out.println("F");
-        }
+ 
+    static boolean GenZ(int var0) {
+       return 1997 < var0;
     }
-}
+ 
+    static void marks(int var0) {
+       if (var0 >= 80) {
+          System.out.println("A+");
+       } else if (var0 >= 75) {
+          System.out.println("A");
+       } else if (var0 >= 70) {
+          System.out.println("A-");
+       } else if (var0 >= 65) {
+          System.out.println("B+");
+       } else {
+          System.out.println("F");
+       }
+ 
+    }
+ }
+ 
